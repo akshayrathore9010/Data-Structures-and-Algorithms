8c04364b441ababc2a2b3fefeedfa7eea76d1552
Akshay Rathore <akshayrathore9010@gmail.com>
2025-02-02 11:01:33 +0530

    Add implementations for printing pairs of elements in arrays

---
+++ 0001_Array/10_PrintLowerPair.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void printPair(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << "( " << arr[i] << ", " << arr[j] << " )" ;
+        }
+    cout<<endl;
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printPair(arr, size);
+
+    return 0;
+}
