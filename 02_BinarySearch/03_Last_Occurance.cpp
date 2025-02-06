6e3dec10e541d699b14e5ca6ffea8248627af12c
Akshay rathore <akshayrathore9010@gmail.com>
2025-02-05 22:23:23 -0800

    Rename function from firstOccurrence to lastOccurrence and update main to reflect the change

--- 02_BinarySearch/03_Last_Occurance.cpp
+++ 02_BinarySearch/03_Last_Occurance.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int firstOccurrence(vector<int>& arr, int target) {
+int lastOccurrence(vector<int>& arr, int target) {
     int n = arr.size();
     int s = 0, e = n - 1, mid;
     int ansIndex = -1;
@@ -26,7 +26,7 @@ int firstOccurrence(vector<int>& arr, int target) {
 int main() {
     vector<int> arr = {1, 2, 2, 2, 3, 4, 5};
     int target = 2;
-    int result = firstOccurrence(arr, target);
+    int result = lastOccurrence(arr, target);
     if (result != -1) {
         cout << "Last occurrence of element " << target << " is at index " << result << endl;
     } else {
