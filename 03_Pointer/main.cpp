a896629ef8052c827c7c4fd2030b5b9be43b2d9c
Akshay rathore <akshayrathore9010@gmail.com>
2025-02-10 22:17:14 -0800

    Add basic pointer demonstration in main.cpp

---
+++ 03_Pointer/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 5;
+    int b = 5;
+    cout << a << endl;
+    cout << b << endl;
+    cout << "Address of a : " << &a << endl;
+    cout << "Address of b : " << &b << endl;
+
+    return 0;
+}
