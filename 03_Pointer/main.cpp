6d1f4d994af88216b4712360f5a508a3f3e7c32a
Akshay rathore <akshayrathore9010@gmail.com>
2025-02-10 22:41:29 -0800

    Update pointer demonstration in main.cpp to include pointer creation and value access

--- 03_Pointer/main.cpp
+++ 03_Pointer/main.cpp
@@ -3,12 +3,24 @@ using namespace std;
 int main()
 {
 
-    int a = 5;
-    int b = 5;
-    cout << a << endl;
-    cout << b << endl;
-    cout << "Address of a : " << &a << endl;
-    cout << "Address of b : " << &b << endl;
+    // int a = 5;
+    // int b = 5;
+    // cout << a << endl;
+    // cout << b << endl;
+    // cout << "Address of a : " << &a << endl;
+    // cout << "Address of b : " << &b << endl;
+
+    // *************************
+
+    int x = 50;
+    // pointer create
+    int *ptr = &x;
+
+    // access the value ptr is pointing to
+    cout << "Address of 'x' is : " << &x << endl;
+    cout << "Value stored at ptr is : " << ptr << endl;
+    cout << "Value ptr is pointing to is : " << *ptr << endl;
+    cout << "Address of ptr : " << &ptr << endl;
 
     return 0;
 }
