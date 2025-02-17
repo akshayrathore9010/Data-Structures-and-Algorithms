3fdf719042c2b8a2ad98bee7b8bc9f2b957d182e
Akshay rathore <akshayrathore9010@gmail.com>
2025-02-17 09:44:51 +0530

    Add Fibonacci sequence implementation using recursion

---
+++ 06_Recursion/05_Fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+
+    int ans = fib(n - 1) + fib(n - 2);
+    return ans;
+}
+
+int main()
+{
+    int ans = fib(9);
+    cout << ans;
+
+    return 0;
+}
