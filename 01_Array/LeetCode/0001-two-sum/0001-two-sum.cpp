128488d85af741a6e02d9816debc0f3dd33079e9
Akshay rathore <akshayrathore9010@gmail.com>
2025-03-13 09:31:04 +0530

    Refactor

--- 01_Array/LeetCode/0001-two-sum/0001-two-sum.cpp
+++ 01_Array/LeetCode/0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,13 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
         vector<int> ans;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
+
+     
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+             
                 if (nums[i] + nums[j] == target) {
                     ans.push_back(i);
                     ans.push_back(j);
@@ -14,6 +15,6 @@ public:
                 }
             }
         }
-        return {};
+        return ans;
     }
 };
--- 01_Array/LeetCode/0136-single-number/0136-single-number.cpp
+++ 01_Array/LeetCode/0136-single-number/0136-single-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0 ; 
-        for(int i =0;i<nums.size();i++){
-            ans = ans ^ nums[i];
+        int n = nums.size();
+
+        int ans = 0;
+        for(int i =0;i<n;i++){
+            ans = ans^nums[i];
         }
         return ans;
     }
